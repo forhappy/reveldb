581798f3d8654694f95b15d55d6ae86eccb1ca48
forhappy <haipingf@gmail.com>
2012-12-12 22:35:41 +0800

    add conf.

---
+++ src/config.c
@@ -0,0 +1,15 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  config.c
+ *
+ *    Description:  reveldb configuration parser.
+ *
+ *        Created:  12/12/2012 10:30:58 PM
+ *
+ *         Author:  Fu Haiping (forhappy), haipingf@gmail.com
+ *        Company:  ICT ( Institute Of Computing Technology, CAS )
+ *
+ * =============================================================================
+ */
+
---
+++ src/config.h
@@ -0,0 +1,19 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  config.h
+ *
+ *    Description:  reveldb configuration parser.
+ *
+ *        Created:  12/12/2012 10:30:20 PM
+ *
+ *         Author:  Fu Haiping (forhappy), haipingf@gmail.com
+ *        Company:  ICT ( Institute Of Computing Technology, CAS )
+ *
+ * =============================================================================
+ */
+
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#endif // _CONFIG_H_
