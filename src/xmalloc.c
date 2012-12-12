b022d3f7d98f9d3e4d4b032e58d4dbfbd4c77c12
forhappy <haipingf@gmail.com>
2012-12-12 22:35:57 +0800

    add xmalloc.

---
+++ src/xmalloc.c
@@ -0,0 +1,15 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  xmalloc.c
+ *
+ *    Description:  xmalloc.
+ *
+ *        Created:  12/12/2012 10:29:29 PM
+ *
+ *         Author:  Fu Haiping (forhappy), haipingf@gmail.com
+ *        Company:  ICT ( Institute Of Computing Technology, CAS )
+ *
+ * =============================================================================
+ */
+
---
+++ src/xmalloc.h
@@ -0,0 +1,19 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  xmalloc.h
+ *
+ *    Description:  xmalloc.
+ *
+ *        Created:  12/12/2012 10:29:18 PM
+ *
+ *         Author:  Fu Haiping (forhappy), haipingf@gmail.com
+ *        Company:  ICT ( Institute Of Computing Technology, CAS )
+ *
+ * =============================================================================
+ */
+
+#ifndef _XMALLOC_H_
+#define _XMALLOC_H_
+
+#endif // _XMALLOC_H_
