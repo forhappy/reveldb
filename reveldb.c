dcf28370b90c102104eb86450b5936c98470f998
forhappy <haipingf@gmail.com>
2012-12-12 10:36:02 +0800

    add reveldb.

---
+++ reveldb.c
@@ -0,0 +1,15 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  reveldb.c
+ *
+ *    Description:  reveldb: REstful leVELDB implementation.
+ *
+ *        Created:  12/11/2012 11:35:58 PM
+ *
+ *         Author:  Fu Haiping (forhappy), haipingf@gmail.com
+ *        Company:  ICT ( Institute Of Computing Technology, CAS )
+ *
+ * =============================================================================
+ */
+
---
+++ reveldb.h
@@ -0,0 +1,18 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  reveldb.h
+ *
+ *    Description:  reveldb: REstful leVELDB implementation.
+ *
+ *        Created:  12/11/2012 11:35:08 PM
+ *
+ *         Author:  Fu Haiping (forhappy), haipingf@gmail.com
+ *        Company:  ICT ( Institute Of Computing Technology, CAS )
+ *
+ * =============================================================================
+ */
+#ifndef _REVELDB_H_
+#define _REVELDB_H_
+
+#endif // _REVELDB_H_
