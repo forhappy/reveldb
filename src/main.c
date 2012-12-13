a03c6fde1c89e1b6c4bc98e4c92379253ec5afc7
forhappy <haipingf@gmail.com>
2012-12-13 17:57:31 +0800

    add src/CMakeLists.txt and main.c, it can be compiled now.

---
+++ src/main.c
@@ -0,0 +1,21 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  main routine.
+ *
+ *        Created:  12/13/2012 05:04:08 PM
+ *
+ *         Author:  Fu Haiping (forhappy), haipingf@gmail.com
+ *        Company:  ICT ( Institute Of Computing Technology, CAS )
+ *
+ * =============================================================================
+ */
+
+#include <reveldb/reveldb.h>
+
+int main(int argc, const char *argv[])
+{
+    return 0;
+}
